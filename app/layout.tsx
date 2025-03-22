4c42e5f63d3d57fc3b0ea58b179a9b869f4f469c
Cansu uğur <99813607+cansu05@users.noreply.github.com>
2025-03-22 18:12:42 +0300

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Zilla_Slab } from "next/font/google";
+import { Zilla_Slab } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Container from "@/components/global/Container";
 import Footer from "@/components/footer/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const zillaSlab = Zilla_Slab({
   variable: "--font-zilla-slab",
