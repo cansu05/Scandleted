8fa1e8e58af092c6b704bc971f45e8ca8f23f660
Cansu uğur <99813607+cansu05@users.noreply.github.com>
2025-03-23 18:36:47 +0300

    bug fixed

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,16 +1,18 @@
 import ProductsContainer from "@/components/products/ProductsContainer";
 
-type PageProps = {
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
+interface ProductsPageProps {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
 
-export default function ProductsPage({ searchParams }: PageProps) {
+const ProductsPage = ({ searchParams }: ProductsPageProps) => {
   const search =
-    typeof searchParams?.search === "string" ? searchParams.search : "";
+    typeof searchParams.search === "string" ? searchParams.search : "";
 
   return (
-    <main className="px-4 lg:px-0">
+    <div>
       <ProductsContainer search={search} />
-    </main>
+    </div>
   );
-}
+};
+
+export default ProductsPage;
