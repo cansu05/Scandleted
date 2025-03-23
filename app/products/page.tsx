59a6d8986c6beeb6dd48550b6ff0112845b43a18
Cansu uğur <99813607+cansu05@users.noreply.github.com>
2025-03-23 19:10:16 +0300

    bug fixed

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,10 @@
 import ProductsContainer from "@/components/products/ProductsContainer";
 
-type Props = {
-  searchParams?: { search?: string };
-};
-
-const ProductsPage = async ({ searchParams }: Props) => {
-  const search = searchParams?.search || "";
-  return <ProductsContainer search={search} />;
-};
-
-export default ProductsPage;
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ search?: string }>;
+}) {
+  const { search } = await searchParams;
+  return <ProductsContainer search={search || ""} />;
+}
