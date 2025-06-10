4b7300e8370490adf72ee98b985a1d9d4d79f31e
Cansu uğur <99813607+cansu05@users.noreply.github.com>
2025-06-10 13:57:58 +0300

    bug fixed

--- components/button/ProductCountButton.tsx
+++ components/button/ProductCountButton.tsx
@@ -25,7 +25,7 @@ const ProductCountButton = ({
     startTransition(() => {
       updateCartItemAction({
         amount: newAmount,
-        cartItemId,
+        cartItemId: cartItemId || "",
       })
         .then(() => {
           onAmountChange(newAmount);
